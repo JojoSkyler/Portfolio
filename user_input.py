38dea365e8e6a3e23ce891692a3a72ddf02ba678
Joan Mbae <kawirajoan06@gmail.com>
2024-02-23 13:19:50 +0300

    Web Technologies

---
+++ user_input.py
@@ -0,0 +1,5 @@
+Name=input("Please enter your name")
+Age=input("Please enter your age")
+Location=input("Please enter your location")
+print(f"Hello" {Name},"you are" {Age} "years old and live in" {Location}.")
+  
