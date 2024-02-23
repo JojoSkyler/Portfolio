0f43d30cbf59e49669706a11451d860dd5568db6
JojoSkyler <159585712+JojoSkyler@users.noreply.github.com>
2024-02-23 13:23:09 +0300

    Delete user_input.py

--- user_input.py
+++
@@ -1,5 +0,0 @@
-Name=input("Please enter your name")
-Age=input("Please enter your age")
-Location=input("Please enter your location")
-print(f"Hello" {Name},"you are" {Age} "years old and live in" {Location}.")
-  
